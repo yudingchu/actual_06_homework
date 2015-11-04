123d25dd73844f7aba975810ba1e449e4885bd26
lpreboot <335304990@qq.com>
2015-11-04 17:25:29 +0800

    homework3

---
+++ 02/lupeng/insert.py
@@ -0,0 +1,14 @@
+num_list = [88,0,1,2,3,123,11,123,421,-124,-2,3,4,5,6]
+sorted_list = []
+for i in range(len(num_list)):
+        if len(sorted_list) == 0:
+            sorted_list.append(num_list[i])
+        else:
+            for j in range(len(sorted_list)):
+                if num_list[i] <= sorted_list[j]:
+                    sorted_list.insert(j, num_list[i])
+                    break
+                elif  j == len(sorted_list) - 1:
+                      sorted_list.insert(j+1, num_list[i])
+                      break
+print sorted_list
