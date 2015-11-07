a12c9bdb97f32fda598e0c019a63e8cb345688e2
songkane <songkane@vip.qq.com>
2015-11-07 10:16:38 +0800

    ok

--- 02/songxiang/kuaisupaixu.py
+++ 02/songxiang/kuaisupaixu.py
@@ -2,9 +2,10 @@
 def kuaisupaixu(num_list,left,right):
 	i = left
 	j = right
-	key = num_list[left]
-	if i == j:
+	if i >= j:
 		return num_list
+
+	key = num_list[left]
 	while j > i:
 		while j > i and num_list[j] >= key:
 			j = j - 1
@@ -15,10 +16,9 @@ def kuaisupaixu(num_list,left,right):
 	kuaisupaixu(num_list,i+1,right)
 	kuaisupaixu(num_list,left,j)
 if __name__ == '__main__':
-	num_list = [2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,45,33,45]
-	'''
-	99999,99999,1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45
-	就不可以
-	'''
+	#num_list = [2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,45,33,45]
+	
+	num_list=[99999,99999,1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
+	
 	kuaisupaixu(num_list,0,len(num_list)-1)
 	print num_list
