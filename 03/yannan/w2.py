27f6a9e91f81ce36493ad4f1358e7e99d0ada6ff
ynzerod <ynzerod@gmail.com>
2015-11-11 19:34:58 +0800

    step.end..?

--- 03/yannan/w2.py
+++ 03/yannan/w2.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 __author__ = 'yannan'
-#作业：第二题统计字符数量之后，打印出现次数前10的字符
+#作业：第二题统计字符数量之后，打印出现次数前10的字符(插入排序)
 read_me = '''first of all, i want make it clear that i can not claim understanding this holy book  in just a few weeks, and i would not dare comment on this sacred book, in addition, i don't think i can give you a full picture of the holy bible in just few words.i can not preach anything here. what i want to do here is to express my understanding by sharing two events described in this book. the fist story i want to share is abandoned tower of babel.according to the bibel,people use the same language to communicate with each other in ancient times.with the soaring vanity,they decided to build a heaven-reaching tower to show off their achievement, god knows, he change the human language into different kinds and make it difficult for people to communicate with each other,hence the failure of building tower of  babel.this story tells people,never do things in selfishness, but make a life out of enternal glory.the other story,before jesus christ was crucified,he said, father,forgive them, for they know not what they do. with great love, he shouldered all the sins of  people. what can we learn from this story?we live in this world thanks to the love of god, for this reanson, we should make our lives glorious to honor our god.finally,i want to sum up by saying that only if we put our lives in the eternal love of god,can we live a perfect life, and  what you appealed is what god expected!'''
 read_me1 = '''a bb ccc dddd eeeee ffffff gggg hhh ii j'''
 
@@ -31,7 +31,6 @@ while m < len(list2):
         list2[n] = list2[n-1]
         n -= 1
     list2[n] = list_top
-print list2
 #print list2
 #打印TOP10
 top = 0
