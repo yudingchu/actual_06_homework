3da6287cc3eef7e8056cacc9f8b38c20a0eb8ba0
songkane <songkane@vip.qq.com>
2015-11-21 03:07:36 +0800

    finally

--- 04/songxiang/chuli.py
+++ 04/songxiang/chuli.py
@@ -1,29 +1,42 @@
 #-*- coding: utf-8 -*-
-
-f = open('www_access_20140823.log','r')
-whole_list = f.readlines()
-f.close()
-#  读取文件
-d = {}
-#  存IP和访问次数
-fnew = open('copy.log','a+')
-final_list=''
-count=0
-for i in whole_list:
-	new=['']*3
-	sigle_list=i.split(' ')
-	new[0]=sigle_list[8]
-	#http状态
-	new[1]=sigle_list[6]
-	#访问url
-	if sigle_list[0] in d:
-		d[sigle_list[0]]=d[sigle_list[0]]+1
-	else:
-		d[sigle_list[0]]=1
-	new[2]=(sigle_list[0],d[sigle_list[0]])
-	#ip,访问次数
-	newstr = str(new)
-	final_list=final_list+newstr+'\n'
-
-fnew.writelines(final_list)
-fnew.close()
+def select_items(whole_list):
+	d_items={}
+	for i in whole_list:
+		new=['']
+		sigle_list=i.split(' ')
+		new[0]=(sigle_list[8],sigle_list[6],sigle_list[0])
+		#http,访问url,ip
+		if new[0] in d_items:
+			d_items[new[0]]=d_items[new[0]]+1
+		else:
+			d_items[new[0]]=1
+	return d_items
+def pick_top10(d_items):
+	whole_list_tuple=sorted(d_items.items(),key=lambda d_items:d_items[1])
+	items_10=''
+	j = 0
+	for i in whole_list_tuple[::-1]:
+		if j < 10:
+			items_10 = items_10+str([i[0][0],i[0][1],(i[0][2],i[1])])+'\n'
+			j = j +1
+		else:
+			return items_10
+def write(item):
+	fnew = open('new.log','a+')
+	fnew.writelines(item)
+	fnew.close()
+if __name__ == '__main__':
+	f = open('www_access_20140823.log','r')
+	whole_list = f.readlines()
+	f.close()
+	#  读取文件
+	d_items = {}
+	d_items = select_items(whole_list)
+	#  选取元素
+	j = 0
+	itmes_top10 = ''
+	itmes_top10 = pick_top10(d_items)
+	#  进行排序,取前十
+	write(itmes_top10)
+	print itmes_top10
+	#  进行写入
